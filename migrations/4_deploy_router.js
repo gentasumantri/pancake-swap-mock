0b50a5e704458e86095ef39162a61bfa409604f3
Genta Sumantri <genta.sumantri@gmail.com>
2021-08-14 06:09:30 +0700

    re-order artifacts

--- migrations/4_deploy_router.js
+++ migrations/4_deploy_router.js
@@ -1,6 +1,6 @@
-const Router = artifacts.require('PancakeRouter02');
 const Factory = artifacts.require('PancakeFactory');
 const WBNB = artifacts.require('Wbnb');
+const Router = artifacts.require('PancakeRouter02');
 
 module.exports = async function (deployer) {
   await deployer.deploy(Router, Factory.address, WBNB.address, { gas: 6721975 });
